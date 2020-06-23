6fadbbe677d2713a356f689d4c0e34614df4eb41
GabyMartinez <37944662+GabyMartinez@users.noreply.github.com>
2020-06-23 18:31:07 -0400

    day 2

---
+++ download_census_data.sh
@@ -0,0 +1,7 @@
+#download census data
+
+url=https://www.census.gov/econ/currentdata/export/csv?programCode=MARTS&timeSlotType=12&startYear=2004&endYear=2020&categoryCode=441&dataTypeCode=SM&geoLevelCode=US&adjusted=no&errorData=no&internal=false
+
+#wget -O hi.csv $url
+
+
